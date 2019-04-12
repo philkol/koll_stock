a3e0e6541e35efb3b28fec1e3ce566fd196900d1
Philipp <kol18485@spengergasse.at>
2019-04-12 06:25:14 +0200

    Create database connection

---
+++ src/test/A_TestConnection.java
@@ -0,0 +1,29 @@
+package test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import connection.OracleConnection;
+import domain.BankAccountException;
+
+public class A_TestConnection {
+
+	public static void main(String[] args) {
+		try //try catch Behandlung
+		{
+			OracleConnection oracon=new OracleConnection(); //Connection instanziert
+			oracon.open(); //öffnen
+			
+			Connection con = oracon.getConnection(); //herholen
+			System.out.println(con.getMetaData().getDatabaseMajorVersion());
+												//DatenbankversionsNr ausgeben
+			oracon.close(); //schließen
+		} 
+		catch (BankAccountException | SQLException e) 
+		{
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+}
